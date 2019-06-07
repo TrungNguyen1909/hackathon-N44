b01200153117df599aaeeedad48c1cecde1bb3e5
Trung Nguyen <ntrung03@outlook.com>
2019-06-07 10:25:55 +0700

    Screen size should be persistent

--- scripts/startup.js
+++ scripts/startup.js
@@ -10,22 +10,22 @@ function enableFont(){
   textFont(font,16);
 }
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
   enableFont()
   background(220);
   startscreen();
   //textSize(15);
   textAlign(CENTER,CENTER)
-  text("Chào mừng đến với trò chơi dò mìn lục giác",200,120)
-  text("Click để mở 1 hình lục giác.",200,140)
-  text("Click phải để cắm cờ vào hình lục giác đó",200,160)
-  text("Nhập độ dài cạnh của lục giác: ", 200, 180);
+  text("Chào mừng đến với trò chơi dò mìn lục giác",400,120*2)
+  text("Click để mở 1 hình lục giác.",400,140*2)
+  text("Click phải để cắm cờ vào hình lục giác đó",400,160*2)
+  text("Nhập độ dài cạnh của lục giác(3≤size≤10): ", 400, 180*2);
 }
 
 function startscreen() {
   background(220);
   input = createInput();
-  input.position(100, 200);
+  input.position(400-input.width/2-25, 200*2);
   button = createButton('PLAY');
   button.position(input.x + input.width, input.y);
   button.mousePressed(getSetting);
@@ -46,7 +46,7 @@ function getSetting() {
     startscreen();
     textSize(15)
     fill('red')
-    text("Độ dài không hợp lệ. Vui lòng nhập lại", 200, 180);
+    text("Độ dài không hợp lệ. Vui lòng nhập lại", 400, 180*2);
   }
 }
 
